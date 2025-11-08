5d4daf273a226f8caadfec9b104d04c06c7d5e87
SolidPebble <shivampokhriyal9987@gmail.com>
2025-11-08 23:41:50 +0530

    com34

--- me.js
+++ me.js
@@ -1,7 +1,5 @@
-let fetchdata=(pd,nd)=>{
-    console.log(pd);
-    setTimeout(()=>{
-        console.log(nd);
-    },2000)
-}
-fetchdata(1,fetchdata(3,4));
+document.querySelectorAll(".box").forEach((m) => {
+  m.addEventListener("click", () => {
+    alert("You Clicked on the moving box");
+  });
+});
